c2a197a0498def495f11a8b1626b32bc92e5c03b
Sean Nam <namsangwoo1@gmail.com>
2020-01-12 17:18:22 -0800

    set up travis settings

---
+++ build.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+# set -e # halt script on error
+
+set -e
+
+DEPLOY_REPO="https://${MY_WEBSITE}@github.com/alice/alice.github.io.git"
+
+function main {
+	clean
+	get_current_site
+	build_site
+	deploy
+}
+
+function clean { 
+	echo "cleaning _site folder"
+	if [ -d "_site" ]; then rm -Rf _site; fi 
+}
+
+function get_current_site { 
+	echo "getting latest site"
+	git clone --depth 1 $DEPLOY_REPO _site 
+}
+
+function build_site { 
+	echo "building site"
+	bundle exec jekyll build --trace
+	bundle exec htmlproofer ./_site --url-ignore "/bit.ly/snam-li/", "https://jasondoesstuff.com/unfollowing-twitter-experiment/"
+}
+
+function deploy {
+	echo "deploying changes"
+
+	if [ -z "$TRAVIS_PULL_REQUEST" ]; then
+	    echo "except don't publish site for pull requests"
+	    exit 0
+	fi  
+
+	if [ "$TRAVIS_BRANCH" != "build" ]; then
+	    echo "except we should only publish the build branch. stopping here"
+	    exit 0
+	fi
+
+	cd _site
+	git config --global user.name "Travis CI"
+    git config --global user.email alice@example.com
+	git add -A
+	git status
+	git commit -m "Lastest site built on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to github"
+	git push $DEPLOY_REPO master:master
+}
+
+main
